ae2755330f0d9f0d61c1eadf6cdd23afe5c2b563
aguslorca <aguslorca.12@gmail.com>
2024-09-11 20:15:20 -0300

    clase 2 javascript del 1.3 al 1.4

--- javascript/clase2/arreglos.js
+++ javascript/clase2/arreglos.js
@@ -1,9 +1,8 @@
 //* Arreglos */
 
-
 //let autos = new Array('Ford','BMW','Fiat'); // sintaxis antigua
 
-const autos = ['Ford','BMW','Fiat'];
+const autos = ["Ford", "BMW", "Fiat"];
 console.log(autos);
 
 // Acceder a un elemento del arreglo con un índice de forma manual
@@ -13,11 +12,27 @@ console.log("autos[2] = " + autos[2]);
 
 // Iterar mediante ciclo for
 
-for(let i = 0; i < autos.length; i++){
-    console.log(i + ": " + autos[i]);
+for (let i = 0; i < autos.length; i++) {
+  console.log(i + ": " + autos[i]);
 }
 
 // Modificar los elementos de un arreglo
 
 autos[1] = "Audi";
 console.log(autos);
+
+// Agregar elementos con push()
+
+autos.push("Peugeot"); // Se agrega al final del arreglo
+console.log(autos);
+
+// Agregar elemento a la última posición usando la longitud del arreglo
+
+autos[autos.length] = "Porsche";
+console.log(autos);
+
+// Preguntar si es un array
+
+console.log(Array.isArray(autos));
+
+console.log(autos instanceof Array);
