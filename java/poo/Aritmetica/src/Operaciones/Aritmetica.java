2ae9218ab6fa8de99fcb88ce4cc51795c0cab40c
aguslorca <aguslorca.12@gmail.com>
2024-09-18 19:43:48 -0300

    clase 4 java

---
+++ java/poo/Aritmetica/src/Operaciones/Aritmetica.java
@@ -0,0 +1,16 @@
+
+package Operaciones;
+
+
+public class Aritmetica {
+     // Atributos de la clase
+    int a;
+    int b;
+    
+    
+    // Método
+    public void sumarNumeros(){
+        int resultado = a + b;
+        System.out.println("resultado = " + resultado);
+    }
+}
