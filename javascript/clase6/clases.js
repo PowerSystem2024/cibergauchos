379ef9398bd37c353c0f8081a222503ca29e23e6
Danilo Pietropaolo <francodanilo136@gmail.com>
2024-09-30 21:07:39 -0300

    clase 6 de js del 6.1 al 6.2

--- javascript/clase6/clases.js
+++ javascript/clase6/clases.js
@@ -21,7 +21,7 @@ class Persona {
   }
 }
 
-let persona1 = new Persona("Mariano", "Bondar", "Buenos Aires");
+let persona1 = new Persona("Mariano", "Bondar");
 console.log(persona1.name);
 console.log(persona1.lastName);
 persona1.name = "Juan";
@@ -29,7 +29,7 @@ console.log(persona1.name);
 persona1.lastName = "Perez";
 console.log(persona1.lastName);
 //console.log(persona1);
-let persona2 = new Persona("Agusto", "Martinez", "Cordoba");
+let persona2 = new Persona("Agusto", "Martinez");
 console.log(persona2.name);
 persona2.name = "Pedro";
 console.log(persona2.name);
