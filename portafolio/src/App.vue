6b1334a00238ea186c45f58a15e3249436755567
Micaela Gonzalez <micaelagonsalez20@gmail.com>
2024-10-10 21:30:05 -0300

    clase 7 portafolio

--- portafolio/src/App.vue
+++ portafolio/src/App.vue
@@ -2,6 +2,7 @@
 import DatosPersonales from './components/DatosPersonales.vue'
 import NavBar from './components/NavBar.vue'
 import Educacion from './components/Educacion.vue';
+import ExperienciaComponente from './components/ExperienciaComponente.vue';
 </script>
 
 <template>
@@ -16,6 +17,7 @@ import Educacion from './components/Educacion.vue';
     <section id="educacion">Educacion - Cursos</section>
     <Educacion />
     <section id="experiencia">Experiencia</section>
+    <ExperienciaComponente />
     <section id="proyectos">Proyectos</section>
     <section id="habilidades">Habilidades</section>
     <section id="intereses">Intereses</section>
